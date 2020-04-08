de3422dc893890eaa9d64fd9a4fb057dde50cea5
Koutei-108 <62491009+Koutei-108@users.noreply.github.com>
2020-04-08 10:33:45 +0900

    webpack mixを導入

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,14 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.copy('resources/js/app.js', 'public/js')
+    .copy('resources/js/bootstrap.js', 'public/js')
+    .copy('resources/js/constants.js', 'public/js')
+    .copy('resources/js/DefGauge.js', 'public/js')
+    .copy('resources/js/newgear.js', 'public/js')
+    .copy('resources/js/newstring.js', 'public/js')
+    .copy('resources/js/util.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css')
+    .sass('resources/sass/index.scss', 'public/css')
+    .sass('resources/sass/newstring.scss', 'public/css')
+;
